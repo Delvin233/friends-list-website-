221bc9eed677343a11cbcb49ca887e9f943d36a3
Delvin233 <yamoahdelvin@gmail.com>
2025-03-08 10:44:38 +0000

    configured app.py for deployment

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,9 @@
 # DONT FROTGET TO UPDATE THE FILE FOR DEPLOYMENT
 
-from flask import Flask
+from flask import Flask, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
 from flask_cors import CORS
+import os
 
 app = Flask(__name__)
 CORS(
@@ -13,7 +14,23 @@ app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///friends.db"
 app.config["SQLALCHEMY_TRACK_NOTIFICATIONS"] = False
 db = SQLAlchemy(app)
 
-import routes  # though routes isnt called in this file, we import it so that we can use the routes created when we run the app.py file
+frontend_folder = os.path.join(os.getcwd(), "..", "frontend")
+dist_folder = os.path.join(os.getcwd(), "..", "frontend", "dist")
+
+
+# serve static files from the "dist " folder under the "frontend" directory
+@app.route("/", defaults={"filename": ""})
+@app.route("/<path:filename>")
+def index(filename):
+    if not filename:
+        filename = "index.html"
+    return send_from_directory(dist_folder, filename)
+
+
+# api routes: which we have done already; GET POST PATCH DELETE
+import routes
+
+# though routes isnt called in this file, we import it so that we can use the routes created when we run the app.py file
 
 # the import is placed here to prevent a cycle error when importing, since it is partially initualized( since we didnt use it :))
 
